04701fabb2c891210551f14e1928e44b1c6f9478
Stevin <stevin_liang@163.com>
2016-06-25 13:26:34 +0800

    src: types.h
    Define some base types. Now only define some, more will added if needed.
    
    Signed-off-by: Stevin <stevin_liang@163.com>

---
+++ src/types.h
@@ -0,0 +1,25 @@
+/*
+ * types.h  Defines base data types used in open j1939 stack.
+ * author: Stevin Liang <stevin_liang@163.co>
+ * created: Jun 25, 2016
+ *
+ * This file is released under GPLv2
+ *
+ */
+
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+typedef unsigned char uint8_t;
+typedef signed char int8_t;
+
+typedef unsigned short uint16_t;
+typedef signed short int16_t;
+
+typedef unsigned int uint32_t;
+typedef signed int int32_t;
+
+typedef signed int size_t;
+
+
+#endif
