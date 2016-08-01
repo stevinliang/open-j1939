a653b14700fc19d2c7b294aaf0b436347f96aed6
Stevin <stevin_liang@163.com>
2016-08-01 22:53:04 +0800

    define j1939 pdu and parameter group related structure.
    Signed-off-by: Stevin <stevin_liang@163.com>

--- src/j1939_dl.h
+++ src/j1939_dl.h
@@ -4,3 +4,24 @@
  *
  *    This file is released under GPLv2
  **/
+
+#include "j1939_dl.h"
+#include "jtypes.h"
+
+struct j1939_pdu {
+	juint8_t prio;
+	juint8_t edp;
+	juint8_t dp;
+	juint8_t pf;
+	juint8_t ps;
+	juint8_t sa;
+	juint8_t len;
+	juint8_t data[8];
+};
+
+struct j1939_pg {
+	juint32_t pgn;
+	juint8_t sa;
+	juint16_t len;
+	void *data;
+};
